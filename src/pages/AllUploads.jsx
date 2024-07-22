bbee3801510e155f0b23129a027aff9512549c22
renji18 <aadarshjha1401@gmail.com>
2024-07-22 20:03:46 +0530

    done fetching stuff

--- src/pages/AllUploads.jsx
+++ src/pages/AllUploads.jsx
@@ -1,18 +1,7 @@
 import React from "react"
-import UploadCircular from "../components/admin/UploadCircular"
-import UploadStudentSheet from "../components/admin/UploadStudentSheet"
-import UploadSyllabus from "../components/admin/UploadSyllabus"
 
 const AllUploads = () => {
-  return (
-    <div className="hidden lg:block my-[100px]">
-      <div className="flex justify-evenly gap-40 mx-40 mb-20">
-        <UploadCircular />
-        <UploadStudentSheet />
-      </div>
-      <UploadSyllabus />
-    </div>
-  )
+  return <div className="hidden lg:block my-[100px]"></div>
 }
 
 export default AllUploads
